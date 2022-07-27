45c5ed6e75ff3f5260fa6c9cea384dfd6aa0f2e5
marquessv <marquessavaldez@gmail.com>
2022-07-26 19:57:47 -0700

    update kitty config

---
+++ .config/kitty/tab_bar.py
@@ -0,0 +1,105 @@
+# pyright: reportMissingImports=false
+
+import datetime
+
+from kitty.fast_data_types import Screen
+from kitty.rgb import Color
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    Formatter,
+    TabBarData,
+    as_rgb,
+    draw_attributed_string,
+    draw_tab_with_powerline,
+)
+from kitty.utils import color_as_int
+
+timer_id = None
+
+
+def calc_draw_spaces(*args) -> int:
+    length = 0
+    for i in args:
+        if not isinstance(i, str):
+            i = str(i)
+        length += len(i)
+    return length
+
+def _draw_left_status(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    return draw_tab_with_powerline(draw_data, screen, tab, before, max_title_length, index, is_last, extra_data)
+
+# more handy kitty tab_bar things:
+# REF: https://github.com/kovidgoyal/kitty/discussions/4447#discussioncomment-2183440
+def _draw_right_status(screen: Screen, is_last: bool) -> int:
+    if not is_last:
+        return 0
+
+    draw_attributed_string(Formatter.reset, screen)
+    date = datetime.datetime.now().strftime(" %H:%M")
+
+    right_status_length = calc_draw_spaces(date + " ")
+
+    draw_spaces = screen.columns - screen.cursor.x - right_status_length
+    if draw_spaces > 0:
+        screen.draw(" " * draw_spaces)
+
+    cells = [
+        (Color(135, 192, 149), date),
+    ]
+
+    screen.cursor.fg = 0
+    for color, status in cells:
+        screen.cursor.fg = as_rgb(color_as_int(color))
+        screen.draw(status)
+    screen.cursor.bg = 0
+
+    if screen.columns - screen.cursor.x > right_status_length:
+        screen.cursor.x = screen.columns - right_status_length
+
+    return screen.cursor.x
+
+
+# REF: https://github.com/kovidgoyal/kitty/discussions/4447#discussioncomment-1940795
+# def redraw_tab_bar():
+#     tm = get_boss().active_tab_manager
+#     if tm is not None:
+#         tm.mark_tab_bar_dirty()
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    # _draw_icon(screen, index, symbol="    ")
+    _draw_left_status(
+        draw_data,
+        screen,
+        tab,
+        before,
+        max_title_length,
+        index,
+        is_last,
+        extra_data,
+    )
+    _draw_right_status(
+        screen,
+        is_last,
+    )
+
+    return screen.cursor.x
